c48ed3facc13dc562a17efe13f939e39a85a845d
Breno Veras <deyvissonbrenoveras@gmail.com>
2025-04-25 01:09:01 -0300

    chore: setup toastify

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,25 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <App />
+      <ToastContainer 
+        position="bottom-right"
+        autoClose={3000} 
+        hideProgressBar={false}
+        newestOnTop={false} 
+        closeOnClick rtl={false} 
+        pauseOnFocusLoss 
+        draggable 
+        pauseOnHover  
+        theme="light"
+      />             
     </Provider>
   </StrictMode>,
 )
